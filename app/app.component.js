f6697adeec89b00aba68544c8c16a0bd8e42e3e2
Artsiom Moladau <dogaz@mail.ru>
2017-04-12 01:11:55 +0300

    new: ночная посиделка

--- app/app.component.js
+++ app/app.component.js
@@ -1,6 +1,7 @@
-function Item(purchase, price, done) {
+function Item(purchase, price, ears, done) {
     this.purchase = purchase;
     this.price = price;
+    this.ears = ears;
     this.done = done;
 }
 
@@ -11,11 +12,19 @@ var AppComponent = ng.core.Component({
     </div>
     <div class="panel">
         <div class="form-inline">
+        
             <div class="form-group">
                 <div class="col-md-8">
                     <input class="form-control" [(ngModel)]="text" placeholder = "Ф.И.О." />
                 </div>
             </div>
+            
+            <div class="form-group">
+                <div class="col-md-8">
+                    <input class="form-control" [(ngModel)]="text2" placeholder = "ears" />
+                </div>
+            </div>
+           
             <div class="form-group">
                 <div class="col-md-6">
                     <input type="number" class="form-control" [(ngModel)]="price" placeholder="Лет" />
@@ -23,7 +32,7 @@ var AppComponent = ng.core.Component({
             </div>
             <div class="form-group">
                 <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
+                    <button class="btn btn-default" (click)="addItem(text,text2,price)">Добавить</button>
                 </div>
             </div>
         </div>
@@ -31,6 +40,7 @@ var AppComponent = ng.core.Component({
             <thead>
                 <tr>
                     <th>ФИО</th>
+                    <th>Ears</th>
                     <th>Возраст</th>
                     <th>Был в Израиле</th>
                 </tr>
@@ -39,6 +49,7 @@ var AppComponent = ng.core.Component({
                 <tr *ngFor="let item of items">
                     <td>{{item.purchase}}</td>
                     <td>{{item.price}}</td>
+                    <td>{{item.ears}}</td>
                     <td><input type="checkbox" [(ngModel)]="item.done" /></td>
                 </tr>
             </tbody>
@@ -54,10 +65,12 @@ var AppComponent = ng.core.Component({
         }
     });
 
-AppComponent.prototype.addItem = function (text, price) {
+AppComponent.prototype.addItem = function (text, price, text2) {
     if (text == null || text == undefined || text.trim() == "")
         return;
+    
     if (price == null || price == undefined)
         return;
-    this.items.push(new Item(text, price));
+
+    this.items.push(new Item(text, price, text2));
 };
